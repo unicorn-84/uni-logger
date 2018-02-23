c2f7ab706264e9234e2aeaa535c28d9b3ef2269f
Vladimir Kapitonov <dev.kapitonov@gmail.com>
2018-02-24 00:35:56 +0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const date = new Date();
-const day = date.getDate();
-const month = date.getMonth() + 1;
-const year = date.getFullYear();
 const dir = path.join(__dirname, 'logs');
-const name = `${day}-${month}-${year}`;
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const seconds = date.getSeconds();
-const pattern = `[${hours}:${minutes}:${seconds}]`;
 
 function appendFile(content, cb) {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const name = `${day}-${month}-${year}`;
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const pattern = `[${hours}:${minutes}:${seconds}]`;
   fs.appendFile(`${dir}/${name}`, `${pattern} ${content}\n`, (error) => {
     if (error) {
       cb(error);
