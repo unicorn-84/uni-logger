43a29ee27961f5690c4090c169e678f4136bb5d0
Vladimir Kapitonov <dev.kapitonov@gmail.com>
2018-02-23 19:29:30 +0300

    Create logger.js

---
+++ logger.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const path = require('path');
+
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const dir = path.join(__dirname, 'logs');
+const name = `${day}-${month}-${year}`;
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const pattern = `[${hours}:${minutes}:${seconds}]`;
+
+function appendFile(content, cb) {
+  fs.appendFile(`${dir}/${name}`, `${pattern} ${content}\n`, (error) => {
+    if (error) {
+      cb(error);
+      return;
+    }
+    cb(null);
+  });
+}
+
+function makeDir(content, cb) {
+  fs.mkdir(dir, (error) => {
+    if (error) {
+      cb(error);
+      return;
+    }
+    appendFile(content, cb);
+  });
+}
+
+function checkDirectory(content, cb) {
+  fs.access(dir, (error) => {
+    if (error && error.code === 'ENOENT') {
+      makeDir(content, cb);
+      return;
+    } else if (error) {
+      cb(error);
+      return;
+    }
+    appendFile(content, cb);
+  });
+}
+
+module.exports.log = (content) => {
+  checkDirectory(content, (error) => {
+    if (error) {
+      console.error(error);
+    }
+  });
+};
